da8c733d6baec5fc0caf571f776efc791ffe9ae1
Amiir <88486212+mugiisha@users.noreply.github.com>
2022-04-13 21:54:58 +0200

    Added CI and initial  test
    Create phantom.yml
    
    added first  unit test

---
+++ src/docs/test/app.test.js
@@ -0,0 +1,20 @@
+import app from '../../../app.js'
+import mocha from 'mocha';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+chai.should();
+chai.use(chaiHttp);
+const { expect } = chai;
+const { it, describe } = mocha;
+
+describe('API testing', () => {
+    it('it should display the welcome message', async () => {
+        const res = await chai.request(app).get('/');
+        expect(res.status).to.be.equal(200);
+        expect(res.body).to.have.property(
+            'message',
+            'Welcome to Phantom Project Powered By Avengers!!',
+            );
+        });
+    });
