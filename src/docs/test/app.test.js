6426861a3da7f092b4a8c69dfd93d307aef82158
Byadiso <byadiso@gmail.com>
2022-04-19 15:24:43 +0200

    adding-sinon-setup

--- src/docs/test/app.test.js
+++ src/docs/test/app.test.js
@@ -1,20 +1,25 @@
-import app from '../../../app.js'
-import mocha from 'mocha';
-import chai from 'chai';
-import chaiHttp from 'chai-http';
+import app from "../../../app.js";
+import mocha from "mocha";
+import chai from "chai";
+import chaiHttp from "chai-http";
+import sinon from "sinon";
 
 chai.should();
 chai.use(chaiHttp);
 const { expect } = chai;
 const { it, describe } = mocha;
 
-describe('API testing', () => {
-    it('it should display the welcome message', async () => {
-        const res = await chai.request(app).get('/');
-        expect(res.status).to.be.equal(200);
-        expect(res.body).to.have.property(
-            'message',
-            'Welcome to Phantom Project Powered By Avengers!!',
-            );
-        });
-    });
+var spy = sinon.spy();
+
+describe("API testing", () => {
+  it("it should display the welcome message", async () => {
+    const res = await chai.request(app).get("/");
+    let spyGetHome = spy(res);
+    expect(res.status).to.be.equal(200);
+    expect(res.body).to.have.property(
+      "message",
+      "Welcome to Phantom Project Powered By Avengers!!"
+    );
+    expect(spyGetHome.called).to.be.true();
+  });
+});
