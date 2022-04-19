7156328375104235f668a8c0c1aa3fbceb85538a
Byadiso <byadiso@gmail.com>
2022-04-19 15:40:02 +0200

    dotenv-general-setup
    dotenv-general-setup
    
    setting up dotenv

--- src/docs/test/app.test.js
+++ src/docs/test/app.test.js
@@ -2,24 +2,20 @@ import app from "../../../app.js";
 import mocha from "mocha";
 import chai from "chai";
 import chaiHttp from "chai-http";
-import sinon from "sinon";
 
 chai.should();
 chai.use(chaiHttp);
 const { expect } = chai;
 const { it, describe } = mocha;
 
-var spy = sinon.spy();
 
 describe("API testing", () => {
   it("it should display the welcome message", async () => {
     const res = await chai.request(app).get("/");
-    let spyGetHome = spy(res);
     expect(res.status).to.be.equal(200);
     expect(res.body).to.have.property(
       "message",
       "Welcome to Phantom Project Powered By Avengers!!"
     );
-    expect(spyGetHome.called).to.be.true();
   });
 });
