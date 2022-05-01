9e46777ef67211df7b6c5efe2ce5231ff359acff
System Administrator <root@andelarwandas-MacBook-Pro.local>
2022-05-01 14:01:42 +0200

    ft(password reset):password resetting Receiving new password Receving reset Token from user check token update user password delete reset token wip

--- src/utils/Email.js
+++ src/utils/Email.js
@@ -11,7 +11,7 @@ const sendEmail = async (options) => {
   })
 
   const emailOptions = {
-    from: "phantom <ukjeando@gmail.com>",
+    from: "phantom <avengersphantom7@gmail.com>",
     to: options.email,
     subject: options.subject,
     text: options.message,
