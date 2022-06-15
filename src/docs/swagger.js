71d49d36df7c49dfcda6db90325e2721a5bdf5ac
kareraolivier <kareraolivier@gmail.com>
2022-06-15 11:37:02 +0200

    new changes

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -1,67 +1,47 @@
-<<<<<<< HEAD
-const userRouteDoc = require('./../Users/Users.docs');
-const roleRouteDoc = require('./../Roles/roles.docs');
-=======
 const userRouteDoc = require("./../Users/Users.docs");
 const roleRouteDoc = require("./../Roles/roles.docs");
 const loginRouteDoc = require("./../Authentication/Login.docs");
 const routeRouteDoc = require("./../Route/route.docs");
->>>>>>> 404273e... documentation and test
 
 const swaggerDoc = {
-  openapi: '3.0.0',
+  openapi: "3.0.0",
   info: {
-    title: 'Phantom Project Powered By Avengers',
-    version: '0.0.1',
-    description: 'This is Avengers Phantom RESTful APIs Documetation'
+    title: "Phantom Project Powered By Avengers",
+    version: "0.0.1",
+    description: "This is Avengers Phantom RESTful APIs Documetation",
   },
   servers: [
     {
-<<<<<<< HEAD
-      url: 'http://localhost:8000',
-      description: 'Local dev Server'
-=======
       url: "http://localhost:9000",
       description: "Local dev Server",
->>>>>>> 404273e... documentation and test
     },
     {
-      url: 'http://www.avengersphantom.com',
-      description: 'production dev server'
-    }
+      url: "http://www.avengersphantom.com",
+      description: "production dev server",
+    },
   ],
   components: {
     securitySchemes: {
       bearerAuth: {
-        type: 'http',
-        scheme: 'bearer',
-        in: 'header',
-        bearerFormat: 'JWT'
-      }
-    }
+        type: "http",
+        scheme: "bearer",
+        in: "header",
+        bearerFormat: "JWT",
+      },
+    },
   },
   security: [
     {
-      bearerAuth: []
-    }
+      bearerAuth: [],
+    },
   ],
 
   tags: [
     {
-      name: 'User',
-      description: 'User Routes'
+      name: "User",
+      description: "User Routes",
     },
     {
-<<<<<<< HEAD
-      name: 'Role',
-      description: 'Role Routes'
-    }
-  ],
-  paths: {
-    ...userRouteDoc,
-    ...roleRouteDoc
-  }
-=======
       name: "Route",
       description: "Bus Routes",
     },
@@ -72,6 +52,5 @@ const swaggerDoc = {
     ...roleRouteDoc,
     ...routeRouteDoc,
   },
->>>>>>> 404273e... documentation and test
 };
 module.exports = swaggerDoc;
